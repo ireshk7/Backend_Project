35449cb29e992298fc232b073c4dee17db282aeb
Iresh Kotabagi <iresh2003@gmail.com>
2025-05-31 14:54:01 +0530

    Saved

--- src/utits/asyncHandler.js
+++ src/utits/asyncHandler.js
@@ -1,7 +1,27 @@
 const asyncHandlers = (requestHandler) => {
-    (req,res,next) => {
+    return  (req,res,next) => {
         Promise.resolve(requestHandler(res,req,next)).catch((err) => next(err))
     }
 }
 
 export {asyncHandlers}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandlers = (requestHandler) =>{
+//     return (req,res,next) => {
+//         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+//     }
+// }
