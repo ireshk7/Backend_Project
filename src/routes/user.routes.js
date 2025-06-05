97090cfd16028e7cee2516e28862b592b062d289
Iresh Kotabagi <iresh2003@gmail.com>
2025-06-05 23:29:20 +0530

    bug

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import {upload} from "../middlewares/multer.middleware.js"
 const router = express.Router()
 
-router.post("/register",registerUser)
+router.post("/register",
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:2
+        }
+    ]),
+    registerUser)
 
 export default router; 
